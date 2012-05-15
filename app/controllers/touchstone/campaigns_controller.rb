ee88a9ac8a01854b5e115270cdddf0144999dbe3
Robin Fisher <robinjfisher@gmail.com>
2012-05-15 16:45:29 +0100

    moved campaigns controller into module rather than namespace

--- app/controllers/touchstone/campaigns_controller.rb
+++ app/controllers/touchstone/campaigns_controller.rb
@@ -1,44 +1,46 @@
-class Touchstone::CampaignsController < ApplicationController
+module Touchstone
+  class CampaignsController < ApplicationController
   
-  layout 'touchstone/touchstone'
+    layout 'touchstone/touchstone'
   
-  def index
-    @campaigns = Campaign.all
-  end
-  
-  def new
-    @campaign = Campaign.new
-  end
-  
-  def create
-    @campaign = Campaign.new(params[:campaign])
-    if @campaign.save
-      flash[:notice] = "Campaign added"
-      redirect_to campaigns_url
-    else
-      flash[:error] = "Could not add campaign"
-      redirect_to new_campaign_url
+    def index
+      @campaigns = Campaign.all
     end
-  end
   
-  def show
-    @campaign = Campaign.find(params[:id])
-    @columns = Touchstone.column_names
-  end
-  
-  def edit
-    @campaign = Campaign.find(params[:id])
-  end
-  
-  def update
-    @campaign = Campaign.find(params[:id])
-    if @campaign.update_attributes(params[:campaign]) && @campaign.save
-      flash[:notice] = "Campaign details updated"
-      redirect_to campaign_path(@campaign)
-    else
-      flash[:error] = "Could not update details at this time."
-      redirect_to campaign_path(@campaign)
+    def new
+      @campaign = Campaign.new
     end
-  end
   
+    def create
+      @campaign = Campaign.new(params[:campaign])
+      if @campaign.save
+        flash[:notice] = "Campaign added"
+        redirect_to campaigns_url
+      else
+        flash[:error] = "Could not add campaign"
+        redirect_to new_campaign_url
+      end
+    end
+  
+    def show
+      @campaign = Campaign.find(params[:id])
+      @columns = Touchstone.column_names
+    end
+  
+    def edit
+      @campaign = Campaign.find(params[:id])
+    end
+  
+    def update
+      @campaign = Campaign.find(params[:id])
+      if @campaign.update_attributes(params[:campaign]) && @campaign.save
+        flash[:notice] = "Campaign details updated"
+        redirect_to campaign_path(@campaign)
+      else
+        flash[:error] = "Could not update details at this time."
+        redirect_to campaign_path(@campaign)
+      end
+    end
+  
+  end
 end
