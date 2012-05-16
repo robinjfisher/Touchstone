7a3dd1a1c07901c91971e6fda3156f9f0a5ed9cb
Robin Fisher <robinjfisher@gmail.com>
2012-05-16 14:53:53 +0100

    add methods to touchstone event

--- app/models/touchstone_event.rb
+++ app/models/touchstone_event.rb
@@ -2,4 +2,16 @@ class TouchstoneEvent < ActiveRecord::Base
   
   attr_accessible :event, :model_name
   
+  def self.signups_in(month,year)
+    start_date = Date.new(year,month,1,0)
+    end_date = start_date.end_of_month
+    where(:created_at => (start_date..end_date), :event => "create")
+  end
+  
+  def self.cancellations_in(month,year)
+    start_date = Date.new(year,month,1,0)
+    end_date = start_date.end_of_month
+    where(:created_at => (start_date..end_date), :event => "destroy")
+  end
+  
 end
